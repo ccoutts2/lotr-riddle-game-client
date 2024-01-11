ddd80f20191bc80866ebe9ef25a144d31e50bb3b
Jack Tomlinson <jtommo66@hotmail.com>
2024-01-11 13:39:20 +0000

    add home page styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,21 @@
+import "./home.scss";
 import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import oneRing from "../../images/oneRing.jpeg";
 
 const Home = () => {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className="home">
+      <h1 className="home__title">gollum's cave</h1>
+      <div className="home__text-wrap">
+        <p className="home__text">
+          You've discovered the entrance to an ominous cave and your eye is
+          drawn to the glimmer of something gold and shiny.
+        </p>
+        <p className="home__text">Maybe you should pick it up?</p>
+      </div>
+      <img className="home__img" src={oneRing}></img>
     </main>
   );
 };
