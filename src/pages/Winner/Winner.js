2db10b22c64605200f2f6d31de8c85c73e8a6ea4
Jack Tomlinson <jtommo66@hotmail.com>
2024-01-11 14:38:01 +0000

    add winner page styling

--- src/pages/Winner/Winner.js
+++ src/pages/Winner/Winner.js
@@ -1,7 +1,22 @@
+import "./winner.scss";
+
 const Winner = () => {
   return (
-    <main>
-      <h1>Hello Winner</h1>
+    <main className="winner">
+      <p className="winner__text">
+        Gollum stumbles back, shocked that you've navigated his tretchery and
+        trickery. "Ask us a riddle, precious, if you're so smart" he croaks. You
+        reply "What's in my pocket?" and slip the ring on, disappearing into
+        thin air as you do so. You turn and leave the cave, triumphant.
+      </p>
+      {/* <p className="winner__text">
+        "Ask us a riddle, precious, if you're so smart"
+      </p>
+      <p className="winner__text">
+        You reply "What's in my pocket?" and slip the ring on, disappearing into
+        thin air as you do so.
+      </p>
+      <p className="winner__text">You turn and leave the cave, triumphant</p> */}
     </main>
   );
 };
