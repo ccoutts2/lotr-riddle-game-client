38bc966218a5b1b967e1ead64d22d4ec0a4a6fb0
Jack Tomlinson <jtommo66@hotmail.com>
2024-01-11 14:29:40 +0000

    add loser styling

--- src/pages/Loser/Loser.js
+++ src/pages/Loser/Loser.js
@@ -1,7 +1,12 @@
+import "./loser.scss";
+
 const Loser = () => {
   return (
-    <main>
-      <h1>Hello Loser</h1>
+    <main className="loser">
+      <p className="loser__text">
+        You choke on your answer and a mist decends over Gollum's eyes. He flies
+        off of his rock, biting down on your flesh. He has the ring.
+      </p>
     </main>
   );
 };
